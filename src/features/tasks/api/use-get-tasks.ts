a134e36aa573c9508b1c9e5b685008a2dd4440d2
Annka-Sarviro <annka_nuha@i.ua>
2024-10-29 16:59:36 +0200

    feat:filters

--- src/features/tasks/api/use-get-tasks.ts
+++ src/features/tasks/api/use-get-tasks.ts
@@ -1,16 +1,36 @@
 import { client } from '@/lib/rpc';
 import { useQuery } from '@tanstack/react-query';
+import { TaskStatus } from '../type';
 
 interface useGetTasksProps {
     workspaceId: string;
+    projectId?: string | null;
+    status?: TaskStatus | null;
+    assigneeId?: string | null;
+    dueDate?: string | null;
+    search?: string | null;
 }
 
-export const useGetTasks = ({ workspaceId }: useGetTasksProps) => {
+export const useGetTasks = ({
+    workspaceId,
+    search,
+    projectId,
+    status,
+    assigneeId,
+    dueDate,
+}: useGetTasksProps) => {
     const query = useQuery({
-        queryKey: ['tasks', workspaceId],
+        queryKey: ['tasks', workspaceId, search, projectId, status, assigneeId, dueDate],
         queryFn: async () => {
             const response = await client.api.tasks.$get({
-                query: { workspaceId },
+                query: {
+                    workspaceId,
+                    projectId: projectId ?? undefined,
+                    search: search ?? undefined,
+                    status: status ?? undefined,
+                    assigneeId: assigneeId ?? undefined,
+                    dueDate: dueDate ?? undefined,
+                },
             });
 
             if (!response.ok) {
